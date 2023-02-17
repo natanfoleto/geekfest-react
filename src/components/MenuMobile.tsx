f6eec158fd0323a28434f53b271f7701ed6c7a35
Natan Foleto <natan@imobpower.com>
2023-02-17 14:30:17 -0300

    Add whatsapp button help!

--- src/components/MenuMobile.tsx
+++ src/components/MenuMobile.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import { SignOut } from "phosphor-react";
+import { SignOut, WhatsappLogo } from "phosphor-react";
 
 import { useAuth } from "../contexts/authentication";
 
@@ -53,7 +53,12 @@ function MenuMobile() {
 
           <aside className={styles.aside}>
             <NavLink to={`/me/${user?.username}`}>Meu perfil</NavLink>
-
+            <a href="http://wa.me/+5517992424418" target="_blank">
+              Help
+              <p>
+                <WhatsappLogo color="#1AD140" size={24} />
+              </p>
+            </a>
             <button onClick={handleSignOut} className={styles.logout}>
               <SignOut />
               Logout
--- src/components/MenuWeb.tsx
+++ src/components/MenuWeb.tsx
@@ -1,5 +1,5 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import { SignOut } from "phosphor-react";
+import { SignOut, WhatsappLogo } from "phosphor-react";
 
 import { useAuth } from "../contexts/authentication";
 
@@ -37,6 +37,12 @@ function MenuWeb() {
 
       <aside className={styles.aside}>
         <NavLink to={`/me/${user?.username}`}>Meu perfil</NavLink>
+        <a href="http://wa.me/+5517992424418" target="_blank">
+          Help
+          <p>
+            <WhatsappLogo color="#1AD140" size={24} />
+          </p>
+        </a>
         <button onClick={handleSignOut} className={styles.logout}>
           <SignOut />
           Logout
